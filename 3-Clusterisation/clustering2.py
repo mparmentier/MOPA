4c7f4f3fe723c06a5659bf9fa3d13b6d6e779037
vagrant <vagrant@ubuntu-14.04-amd64-vbox>
2016-12-01 11:27:58 +0000

    began characterisation of clusters

--- 3-Clusterisation/clustering2.py
+++ 3-Clusterisation/clustering2.py
@@ -12,10 +12,11 @@ from sklearn.pipeline import make_pipeline
 
 
 NB_CLUSTERS = 30
-MONGO_LIMIT = 500
+MONGO_LIMIT = 2000
 
-
-
+##############################
+## Database
+##############################
 
 # Connexion
 try:
@@ -42,12 +43,28 @@ try:
 except Exception as e:
     raise Exception("There is an error when querying mongo : " + e.message)
 
-# ceci s'appelle en python une "comprehension".
-# c'est tres pratique, on écrit en une ligne une boucle FOR pour créer une liste:
-# https://docs.python.org/2/tutorial/datastructures.html#list-comprehensions
 # Cette ligne va retourner une liste de dictionnaires
-texts = [{'id':r['ANNONCE']['GESTION']['REFERENCE']['IDWEB'], 'text':r['ANNONCE']['DONNEES']['OBJET']['OBJET_COMPLET']} for r in result]
-print(len(texts))
+#texts = [{'id':r['ANNONCE']['GESTION']['REFERENCE']['IDWEB'], 'text':r['ANNONCE']['DONNEES']['OBJET']['OBJET_COMPLET']} for r in result]
+#print(len(texts))
+
+texts = []
+for r in result:
+    if 'OBJET_COMPLET' in r['ANNONCE']['DONNEES']['OBJET']:
+        t = r['ANNONCE']['DONNEES']['OBJET']['OBJET_COMPLET']
+    # parfois, 'objet_complet' est une liste...
+    elif type(r['ANNONCE']['DONNEES']['OBJET']) == list:
+        objets_complets = [oc['OBJET_COMPLET'] for oc in r['ANNONCE']['DONNEES']['OBJET']]
+        # on concatène les descriptions en une seule string
+        t = ' '.join(objets_complets)
+    i = r['ANNONCE']['GESTION']['REFERENCE']['IDWEB']
+    obj = {'id':i,'text':t}
+    texts += [obj]
+
+
+
+##############################
+## Clustering
+##############################
 
 # conversion de texts en DataFrame. Cette ligne va convertir 'texts' en un tableau (dataframe) avec 2 colones : id et text
 texts_df = pd.DataFrame(texts)
@@ -76,5 +93,39 @@ km = MiniBatchKMeans(n_clusters=NB_CLUSTERS, init='k-means++', max_iter=1000, n_
 # on lance l'algo sur la data
 km.fit(X)
 
-print(km.labels_)
+# recuperation des labels
+cluster_ids = km.labels_
+
+
+
+##############################
+## Aide a la caracterisation des clusters
+##############################
+
+# retourne le top N des "features" donc la valeur est donnée dans "values"
+def pickTopN(values, features, N):
+    ind = pickTopNIndexes(values, N)
+    return [features[i] for i in ind]
+
+# retourne les indices des N nombres les plus grands dans "arr"
+def pickTopNIndexes(arr, N):  # optimisable
+    return arr.argsort()[-N:][::-1]
+
+
+# on crée une copie de texts_df
+labelled_texts = pd.DataFrame(texts_df)
+# on ajoute une colonne contenant l'identifiant du cluster
+labelled_texts['cluster_id'] = ["cl_" + str(i) for i in cluster_ids]
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(stop_words=stop_words)
+X = vectorizer.fit_transform(texts_df['text'])
+X = pd.DataFrame(X.toarray())
+
+X = X.groupby(cluster_ids).apply(sum)
+features = vectorizer.get_feature_names()
+
+o = X.apply(lambda arr: pickTopN(arr, features, 15), 1)
+
+print(o)
 
